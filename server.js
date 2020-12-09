13d83d09bf97ee221eed97e9331064a6a2da01ab
James Baldwin <jamescbaldwin@gmail.com>
2020-12-09 17:37:32 -0500

    handlebars/sellers page init commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require("express-session");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
 require("dotenv").config();
+console.log("OUR group's name is ", process.env.SECRET_TEAM);
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
